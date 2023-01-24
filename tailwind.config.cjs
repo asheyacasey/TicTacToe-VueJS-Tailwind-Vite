9459a0fd8b1c7a770a85e105fd5158c9fcc39c8a
Ashy <asheya.yuson@usjr.edu.ph>
2023-01-24 16:27:13 +0800

    added new project

---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+
+      },
+      fontFamily: {
+        'Inter' : [ 'Inter', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
